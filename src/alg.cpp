83226d5001377542e7608f57d26f1929acb97974
Малышева Галина <99182939+Malychwa@users.noreply.github.com>
2022-03-31 21:02:41 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -22,7 +22,6 @@ int priority(char ch) {
   }
 
 
-
 std::string infx2pstfx(std::string inf) {
   std::string postf;
   TStack <char, 100> stack1;
@@ -49,10 +48,45 @@ std::string infx2pstfx(std::string inf) {
       }
     }
   }
+  while (!stack1.isEmpty()) {
+    postf.push_back(stack1.get());
+    stack1.pop();
+  }
   return postf;
 }
-
-int eval(std::string pref) {
-  // добавьте код
-  return 0;
+  
+int calcul(char ch, int a, int b) {
+switch (ch) {
+  case '+':
+    return (a + b);
+  case '-':
+    return (a - b);
+  case '*':
+    return (a * b);
+  case '/':
+    if (b != 0) {
+      return a / b;
+    }
+  default: return 0;
+  }
 }
+  
+  
+int eval(std::string pref) {
+  TStack <int, 100> stack2;
+  int res = 0;
+  for (int i = 0; i < pref.size(); i++) {
+    if (priority(pref[i]) == -1) {
+      stack2.push(pref[i] - '0');
+    } else if (priority(pref[i]) <= 3) {
+      int op2 = stack2.get();
+      stack2.pop();
+      int op1 = stack2.get();
+      stack2.pop();
+      stack2.push(calcul(pref[i], op1, op2));
+    }
+  }
+  res = stack2.get();
+  return res;
+}
+  
